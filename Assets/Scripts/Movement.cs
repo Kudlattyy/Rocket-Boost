aa498f5a96b70aad97956cdbe4a10b60b52bac26
Hubert Suchorowski <hubert.suchorowski1@gmail.com>
2025-02-05 10:54:17 +0100

    Added simple script for audio

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,14 +9,18 @@ public class Movement : MonoBehaviour
     [SerializeField] InputAction thrust;
     [SerializeField] InputAction rotation;
     [SerializeField] float thrustPower = 5;
-
     [SerializeField] float rotationPower = 100;
+
+    bool a_play; 
+    bool a_change; 
     Rigidbody rb;
+    AudioSource audioSource;
 
 
     void Start() 
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void FixedUpdate() 
@@ -34,7 +38,13 @@ public class Movement : MonoBehaviour
         if(thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustPower *Time.fixedDeltaTime);
+            if(!audioSource.isPlaying){
+                audioSource.Play();
+            }
         }
+        else{
+                audioSource.Stop();
+            }
     }
     private void Rotation()
     {
@@ -52,4 +62,5 @@ public class Movement : MonoBehaviour
         transform.Rotate(Vector3.forward * rotationFrame * Time.fixedDeltaTime);
         rb.freezeRotation = false;
     }
+
 }
